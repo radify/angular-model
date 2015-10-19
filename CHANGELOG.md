4c00f8f07a6771b80ded617959fa83f41038e3d9
John F. Mercer <john@radify.io>
2015-10-19 15:10:31 -0400

    Adds gulp; Updates various things
    This commit adds gulp to `angular-model` and adds a significant
    number of tasks.
      * Uses `run-sequence` to manage gulp tasks
      * Fixes coveralls support
        * Removes coveralls task from karma.conf.js
        * Adds coveralls task to gulp
      * Adds jscs task and .jscsrc
      * Adds jshint task and .jshintrc
      * Adds security audit task with `requireSafe`
      * Adds CodeClimate coverage and .codeclimate.yml
        * N.B. CodeClimate checks only `angular-model.js` and
          no other files.
      * Removes previous build script, which is now superfluous.
    
    Additional changes
      * Changes style in the following:
        * `src/angular-model.js`
        * `spec/modelSpec.js`
        * `config/karma.conf.js`
        * `README.md`
      * `package.json` changes
        * Updates gulpfile packages to latest versions
        * Bumps version number to 0.5.0
        * Updates Nate's and Warren's contact info
        * Updates repository location
          * Changes uor to radify
        * Adds John as a contributor
          * This is necessary to allow John to publish `angular-model`
            to NPM
        * Adds keywords section
      * Moves `modelSpec.js` from `test/` to `spec/`
      * Deletes `coveralls.yml`, which is superfluous
      * Adds badges to README
        * Dependencies (david-dm.org)
        * CodeClimate score
      * Edits `CHANGELOG.md`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,27 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
-## 0.4.1 - 2015-08-19
+## [0.5.0] - 2015-10-31
+### Added
+- Gulp
+- CodeClimate reporting and badge
+- runSequence for Gulp
+- jscs for code style
+- jshint for linting
+- dependency badges
+### Changed
+- Gulp, rather than Karma, handles Coveralls reporting
+### Fixed
+- Coveralls reporting and badge
+### Security
+- Adds requireSafe security check to Gulp
+### Updated
+- Updates package.json
+- Style updates in `angular-model.js`
+
+## [0.4.1] - 2015-08-19
 * Adds a CHANGELOG
+
+[Unreleased]: https://github.com/radify/angular-model/compare/v0.5.0...HEAD
+[0.5.0]: https://github.com/radify/angular-model/compare/v0.4.1...v0.5.0
+[0.4.1]: https://github.com/radify/angular-model/compare/v0.4.0...v0.4.1
---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp      = require('gulp');
+var Server    = require('karma').Server;
+var coveralls = require('gulp-coveralls');
+var jshint    = require('gulp-jshint');
+var jscs      = require('gulp-jscs');
+var runSequence = require('run-sequence');
+var gulpRequireSafe = require('gulp-requiresafe');
+
+/*
+ * PLEASE NOTE: run-sequence is a
+ * temporary solution until the release of
+ * Gulp 4.0, which contains run-sequence's
+ * functionality.
+ */
+
+var paths = {
+  'spec': 'spec/**/*.js',
+  'src': 'src/**/*.js'
+};
+
+gulp.task('test', function(done) {
+  new Server({
+    configFile: __dirname + '/config/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('coveralls', function() {
+  gulp.src('coverage/**/lcov.info')
+    .pipe(coveralls());
+});
+
+gulp.task('lint', function() {
+  return gulp.src([paths.src])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('style', function() {
+  return gulp.src([paths.src, paths.spec])
+    .pipe(jscs())
+    .pipe(jscs.reporter());
+});
+
+gulp.task('security', function(cb) {
+  gulpRequireSafe({package: __dirname + '/package.json', stopOnError: true}, cb);
+});
+
+gulp.task('default', function(cb) {
+  runSequence('test', ['lint', 'style', 'coveralls', 'security'], cb);
+});
